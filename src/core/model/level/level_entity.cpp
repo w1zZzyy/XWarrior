0b45503db62ccd8f269f2806520439f582a0d445
w1zZzyy <dkilikki22@gmail.com>
2025-12-31 18:10:44 +0000

    rm id

--- src/core/model/level/level_entity.cpp
+++ src/core/model/level/level_entity.cpp
@@ -3,15 +3,9 @@
 namespace core {
 namespace model {
 
-Level::Level(value_object::LevelID id, value_object::LevelValue val,
-             value_object::LevelType type,
+Level::Level(value_object::LevelValue val, value_object::LevelType type,
              value_object::Experience req_exp) noexcept
-    : id_(id), val_(val), type_(type), required_exp_(req_exp) {}
-
-Level::Build& Level::Build::setId(value_object::level_id id) noexcept {
-  id_.emplace(id);
-  return *this;
-}
+    : val_(val), type_(type), required_exp_(req_exp) {}
 
 Level::Build& Level::Build::setValue(value_object::level_value val) noexcept {
   val_.emplace(val);
@@ -30,10 +24,10 @@ Level::Build& Level::Build::setRequiredExp(
 }
 
 std::optional<Level> Level::Build::build() const noexcept {
-  if (!id_ || !val_ || !type_ || !exp_) {
+  if (!val_ || !type_ || !exp_) {
     return std::nullopt;
   }
-  return Level(id_.value(), val_.value(), type_.value(), exp_.value());
+  return Level(val_.value(), type_.value(), exp_.value());
 }
 
 }  // namespace model
--- src/core/model/level/level_entity.hpp
+++ src/core/model/level/level_entity.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "../../value_object/level/level_id.hpp"
 #include "../../value_object/level/level_type.hpp"
 #include "../../value_object/level/level_value.hpp"
 #include "../../value_object/shared/experience.hpp"
@@ -11,25 +10,21 @@ namespace core {
 namespace model {
 
 class Level {
-  value_object::LevelID id_;
   value_object::LevelValue val_;
   value_object::LevelType type_;
   value_object::Experience required_exp_;
-  Level(value_object::LevelID id, value_object::LevelValue val,
-        value_object::LevelType type,
+  Level(value_object::LevelValue val, value_object::LevelType type,
         value_object::Experience req_exp) noexcept;
 
  public:
   class Build {
    public:
-    Build& setId(value_object::level_id id) noexcept;
     Build& setValue(value_object::level_value val) noexcept;
     Build& setType(value_object::LevelType type) noexcept;
     Build& setRequiredExp(value_object::experience_value exp) noexcept;
     std::optional<Level> build() const noexcept;
 
    private:
-    std::optional<value_object::LevelID> id_;
     std::optional<value_object::LevelValue> val_;
     std::optional<value_object::LevelType> type_;
     std::optional<value_object::Experience> exp_;
@@ -38,7 +33,6 @@ class Level {
   constexpr bool canUpgrade(value_object::Experience exp) const noexcept {
     return exp.hasEnough(required_exp_);
   }
-  constexpr value_object::LevelID id() const noexcept { return id_; }
   constexpr value_object::LevelValue value() const noexcept { return val_; }
   constexpr value_object::LevelType type() const noexcept { return type_; }
 };
